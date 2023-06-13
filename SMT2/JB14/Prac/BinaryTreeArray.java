a999c7e1e84c49c23afd9c0869359821a248c254
Rechra <hazumiya.yc@gmail.com>
2023-06-13 20:43:22 +0700

    hiya

--- SMT2/JB14/Prac/BinaryTreeArray.java
+++ SMT2/JB14/Prac/BinaryTreeArray.java
@@ -25,4 +25,37 @@ public class BinaryTreeArray
             traverseInOrder(2 * idxStart + 2);
         }
     }
+
+    void add(int data)
+    {
+        int currentIdx = 0;
+        while (true)
+        {
+            if (currentIdx >= idxLast) break;
+            if (data > this.data[currentIdx]) currentIdx = currentIdx * 2 + 2;
+            else if (data < this.data[currentIdx]) currentIdx = currentIdx * 2 + 1;
+            else break;
+        }
+        this.data[currentIdx] = data;
+    }
+
+    void traversePreOrder(int idxStart)
+    {
+        if (idxStart <= idxLast)
+        {
+            System.out.print(data[idxStart] + " ");
+            traversePreOrder(2 * idxStart + 1);
+            traversePreOrder(2 * idxStart + 2);
+        }
+    }
+
+    void traversePostOrder(int idxStart)
+    {
+        if (idxStart <= idxLast)
+        {
+            traversePostOrder(2 * idxStart + 1);
+            traversePostOrder(2 * idxStart + 2);
+            System.out.print(data[idxStart] + " ");
+        }
+    }
 }
